7f6305dbf5d25326d5b172ba5f97fc223c93c846
david_who <david_who@aliyun.com>
2017-03-01 22:26:06 +0800

    first commit

---
+++ orderit.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Bob’s Auto Parts - Order Results</title>
+</head>
+<body>
+	<h1>Bob’s Auto Parts</h1>
+	<h2>Order Results</h2>
+	<?php echo "<p>Order processed.</p>"; ?>
+</body>
+</html>
